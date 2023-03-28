b2cb94b52316abdad53e1f6258268e00a157adc2
ghmanjurr <manjurr.ghalib@gmail.com>
2023-03-27 17:20:23 -0700

    button types example react-bootstrap

--- react-bootstrap/react-bootstrap/src/index.js
+++ react-bootstrap/react-bootstrap/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TypesExample from './ButtonTypes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <TypesExample />
+  
   </React.StrictMode>
 );
 
